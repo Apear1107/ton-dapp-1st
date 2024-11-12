57b75106eca7da270e180a1dda1d103a88332957
zgp <zgp513@163.com>
2024-11-12 18:35:55 +0800

    new:deploy-to-git,config

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,10 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+const manifestUrl = 'https://github.com/Apear1107/ton-dapp-1st/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
-  </TonConnectUIProvider>,
+  </TonConnectUIProvider>
 )
