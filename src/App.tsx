2460a471fc098fd510d101d0ef7b1fcac5bb517d
zgp <zgp513@163.com>
2024-11-13 14:38:29 +0800

    new:twa-dev/sdk,show alert

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css';
 import { TonConnectButton } from '@tonconnect/ui-react';
 import { useMainContract } from './hooks/useMainContract'
 import { useTonConnect } from './hooks/useTonConnect';
+import { fromNano } from '@ton/core';
+import WebApp from '@twa-dev/sdk';
 
 function App() {
   // 合约数据
@@ -13,8 +15,11 @@ function App() {
     sendDeposit,
     sendWithdrawalRequest,
   } = useMainContract();
-  // 发送发连接状态
+  // 钱包连接状态
   const { connected } = useTonConnect();
+  const showAlert = () => {
+    WebApp.showAlert("Hey there!");
+  };
 
   return (
     <div>
@@ -23,20 +28,28 @@ function App() {
       </div>
       <div>
         <div className='Card'>
-          {/* <b>Our contract owner Address</b>
-          <div className='Hint'>{owner_address?.toString()}</div> */}
+          <b>{WebApp.platform}</b>
           <b>Our contract Address</b>
           <div className='Hint'>{contract_address}</div>
           <b>Our contract Balance</b>
-          <div className='Hint'>{contract_balance}</div>
-          {/* <b>Recent sender Address</b>
-          <div className='Hint'>{recent_sender?.toString()}</div> */}
-        </div>
+          {contract_balance && (
+            <div className='Hint'>{fromNano(contract_balance)}</div>
+          )}        </div>
 
         <div className='Card'>
           <b>Counter Value</b>
           <div>{counter_value ?? "Loading..."}</div>
         </div>
+
+        <a
+          onClick={() => {
+            showAlert();
+          }}
+        >
+          Show Alert
+        </a>
+
+        <br />
 
         {connected && (
           <a
